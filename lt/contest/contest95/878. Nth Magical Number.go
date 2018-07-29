77e5b9da04fabb34e58de451233c44eee985d486
glc <gaolichuang@gmail.com>
2018-07-29 17:30:00 +0800

    878 gcd

--- lt/contest/contest95/878. Nth Magical Number.go
+++ lt/contest/contest95/878. Nth Magical Number.go
@@ -2,13 +2,15 @@ package main
 
 import (
         "fmt"
-        "math"
 )
 
 func main() {
-        //fmt.Println(nthMagicalNumber(1, 2, 3))//2
-        //fmt.Println(nthMagicalNumber(4, 2, 3))//6
-        fmt.Println(nthMagicalNumber(5, 2, 3))//10
+        fmt.Println(nthMagicalNumber(1, 2, 3))//2
+        fmt.Println(nthMagicalNumber(4, 2, 3))//6
+        fmt.Println(nthMagicalNumber(5, 2, 4))//10
+        fmt.Println(nthMagicalNumber(3,6,4))//10
+        fmt.Println(nthMagicalNumber(1000000000,40000, 40000))//10
+        fmt.Println(nthMagicalNumber(1000000000,40000, 40000))//10
 }
 /*
   int nthMagicalNumber(int N, int A, int B) {
@@ -22,46 +24,24 @@ func main() {
         return l % (long)(pow(10, 9) +7);
     }
 */
-func nthMagicalNumber(N,A,B int) int {
-        var a,b,t,l,r float64 = float64(A),float64(B),0,2,math.Pow(10,14)
-        for b!= 0 {
-                t=a
-                a=b
-                b=float64(int(t)%int(b))
-        }
+
+
+func nthMagicalNumber(N int, A int, B int) int {
+        if A>B {A,B=B,A}
+        lcm := A*B/gcd(A,B)
+        l,r := A,B*N
         for l<r {
-                var m float64 = (l+r)/2
-                if int(m)/(A)+int(m)/(B)-int(m)/(A*B/gcd(A,B)) < (N) {
+                m := (l+r)/2
+                c := m/A+m/B-m/lcm
+                if c <N {
                         l=m+1
                 } else {
                         r=m
                 }
         }
-        return int(r)
+        return r%1000000007
 }
 
-
-func nthMagicalNumber1(NN int, AA int, BB int) int {
-        var A, B, N float64 = float64(AA), float64(BB), float64(NN)
-        if A > B {
-                A, B = B, A
-        }
-        var lcm float64 = float64(AA*BB/gcd(AA,BB))
-        fmt.Println(AA,BB,lcm)
-        var l, r float64 = A, B*N*A
-        //fmt.Println(l,r)
-        for l < r {
-                var m float64 = l + (r - l) / 2
-                counter := m / A + m / B - m /lcm
-                fmt.Println("idx",m,l,r,counter)
-                if counter >= N {
-                        r = m
-                } else {
-                        l = m + 1
-                }
-        }
-        return int(r)
-}
 func gcd(A, B int) int {
         if (A < B) {
                 A, B = B, A
