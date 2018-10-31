30f6f940f0824ef174ee9d4254d3c0b70eb924d2
glc <gaolichuang@gmail.com>
2018-10-31 10:28:03 +0800

    update

--- bit/bit.go
+++ bit/bit.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-        fmt.Println(1^1)
-        fmt.Println(0^1)
-        fmt.Println(1^0)
-        fmt.Println(0^0)
-    x := 9
-    fmt.Println(^x+1)
-    fmt.Println(^(x-1))
-    fmt.Println(x&(-x))
+	fmt.Println(1 ^ 1)
+	fmt.Println(0 ^ 1)
+	fmt.Println(1 ^ 0)
+	fmt.Println(0 ^ 0)
+	x := 9
+	fmt.Println(^x + 1)
+	fmt.Println(^(x - 1))
+	fmt.Println(x & (-x))
 
 }
