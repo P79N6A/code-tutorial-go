817b2b73be76ba0bc78106a9360a0695f66cd341
glc <gaolichuang@gmail.com>
2018-11-23 16:47:58 +0800

    DI string

--- lt/contest/contest111/942. DI String Match.go
+++ lt/contest/contest111/942. DI String Match.go
@@ -35,23 +35,39 @@ func main() {
 }
 /*
 如何保证原有的DI关系不变?这样才能线性递归下去...
-好像有点排列的意思
+已有序列都+1并不影响ID关系
 */
-func diStringMatch(S string) []int {
-    return solve(S)
-}
-func solve(s string) []int {
+func diStringMatch(s string) []int {
     if s=="I" {return []int{0,1}}
     if s=="D" {return []int{1,0}}
-    ans := solve(s[:(len(s)-1)])
+    ans := diStringMatch(s[:(len(s)-1)])
     if s[len(s)-1] == 'I' {
         ans = append(ans, len(s))
     } else {
         //如果是D,全都+1还是保持原来顺序,把0 append到最后即可
         for i:=0;i<len(ans);i++ {
             ans[i] += 1
-        }
+        }// 维持原有的ID关系；都+1是不变的
         ans = append(ans,0)
     }
+    return ans
+}
+
+func diStringMatch2(S string) []int {
+    ans := make([]int,0,len(S)+1)
+    left,right := 0,len(S)
+    /*
+    从0-n的两头填数据，这样填入的不是最大就是最小；新加入的数据对原有的ID结构不产生影响
+    */
+    for i:=0;i<len(S);i++ {
+        if S[i]=='I' {
+            ans = append(ans,left)
+            left+=1
+        } else {
+            ans = append(ans,right)
+            right-=1
+        }
+    }
+    ans = append(ans,left)
     return ans
 }
